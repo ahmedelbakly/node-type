94ab3fc366e3ad632faf08fe7b269ffacd86aeaf
ahmed elbakly <78669298+ahmedelbakly@users.noreply.github.com>
2025-04-25 01:02:30 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import authRoutes from './routes/authRoutes.js';
+import express from 'express'
+import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
+import authRoutes from './routes/authRoutes.js'
 
-
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
 // Connect to MongoDB
-connectDB();
+connectDB()
 
-app.use(express.json());
+app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
+  res.send(
+    'Hello World from the backend with typescript and express and mongodb'
+  )
+})
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
